0c68bd5ffe335e172b939129d5a4b864a93e0030
Nso Arrey <spencerice149@gmail.com>
2023-11-03 19:00:45 -0700

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 import variable_load_5
-
-
-def variable_load():
-    print(variable_load_5.a)
-
-
 if __name__ == "__main__":
-    variable_load()
+    print(variable_load_5.a)
