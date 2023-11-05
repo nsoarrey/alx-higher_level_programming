ff05bdc4e47dbbafad71a3cbcda3188cea6cfc04
Nso Arrey <spencerice149@gmail.com>
2023-11-04 19:27:49 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""print_list_integer: prints all integers of a list.
-   Args:
-       my_list: list of integers to be summed.
-"""
-
 
 def print_list_integer(my_list=[]):
     for i in my_list:
-        print("{:d}".format(i))
+        print('{:d}'.format(i))
