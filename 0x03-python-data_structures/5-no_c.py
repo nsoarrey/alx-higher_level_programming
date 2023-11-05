20797ec5e30b149988617726a625c9cd3dab649e
Nso Arrey <spencerice149@gmail.com>
2023-11-05 07:57:51 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if my_string:
-        L = list(my_string)
-        L1 = ["c", "C"]
-        for i in range(len(L) - 1):
-            if L[i - 1] in L1:
-                del L[i - 1]
-        my_string = "".join(L)
-    return my_string
+    updated_str = ''
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            updated_str += i
+    return (updated_str)
