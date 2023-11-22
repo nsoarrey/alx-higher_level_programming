52ccbf019033d2bef4f6a21c29b18fce6bec7d01
Nso Arrey <spencerice149@gmail.com>
2023-11-22 00:58:39 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """a class Square that defines a square"""
+
+    def __init__(self, size):
+        """__init__ method
+        Args:
+            size: size of square
+        """
+        self._size = size
